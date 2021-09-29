62fc46443e524895af19a28a2ed36c087ddf03f6
jb-abbadie <jb-abbadie@users.noreply.github.com>
2021-09-29 21:23:20 +0200

    Simplify Unzip function

--- pkg/files.go
+++ pkg/files.go
@@ -37,54 +37,61 @@ func CheckFileExist(file string) bool {
 
 // Unzip will decompress a zip archive, moving all files and folders
 // within the zip file (parameter 1) to an output directory (parameter 2).
-func Unzip(src string, dest string) ([]string, error) {
+func Unzip(src string, dest string) error {
 	r, err := zip.OpenReader(src)
 	if err != nil {
-		return []string{}, err
+		return err
 	}
 	defer r.Close()
 
-	filenames := make([]string, 0, len(r.File))
-
 	for _, f := range r.File {
-		rc, err := f.Open()
+		err := handleZipFile(f, dest)
 		if err != nil {
-			return filenames, err
-		}
-		defer rc.Close()
-
-		// Store filename/path for returning and using later on
-		fpath := filepath.Join(dest, f.Name)
-		filenames = append(filenames, fpath)
-
-		if f.FileInfo().IsDir() {
-			// Make Folder
-			err := os.MkdirAll(fpath, os.ModePerm)
-			if err != nil {
-				return []string{}, err
-			}
-		} else {
-			// Make File
-			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
-				return filenames, err
-			}
-
-			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return filenames, err
-			}
-
-			_, err = io.Copy(outFile, rc)
-
-			// Close the file without defer to close before next iteration of loop
-			outFile.Close()
-
-			if err != nil {
-				return filenames, err
-			}
+			return err
 		}
 	}
-	return filenames, nil
+	return nil
+}
+
+// handle 1 zip file
+func handleZipFile(f *zip.File, dest string) error {
+	rc, err := f.Open()
+	if err != nil {
+		return err
+	}
+	defer rc.Close()
+
+	// Store filename/path for returning and using later on
+	fpath := filepath.Join(dest, f.Name)
+
+	if f.FileInfo().IsDir() {
+		// Make Folder
+		err := os.MkdirAll(fpath, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	// Make File
+	if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+		return err
+	}
+
+	outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(outFile, rc)
+
+	// Close the file without defer to close before next iteration of loop
+	outFile.Close()
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // CreateDirIfNotExist : create directory if directory does not exist
